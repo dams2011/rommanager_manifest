128123e28675d772b4edf1c824bad61703976c85
dams2011 <marcoastorga.g@gmail.com>
2012-08-01 19:56:46 -0400

    test

---
+++ manifest.js
@@ -0,0 +1,143 @@
+{
+    "version": "1",
+    "homepage": "http://www.madteam.co",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=UJKH4W8TDQ6WS&lc=IE&item_name=Donations&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted",
+    "roms": [
+        {
+            "label": "CM7 ROMs for Samsung mimmi",
+            "name": "CyanogenMod 7.2-RC0 (v2.1)",
+            "summary": "[fake - update test] (20120207)",
+            "visible": "false",
+            "product": "gingerbread-nightly",
+            "modversion": "CyanogenMod-7-20120207-MADTEAM-galaxy5",
+            "incremental": "20120207",
+            "device": "galaxy5",
+            "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/update-cm-7-20120207-MADTEAM-galaxy5.zip",
+            "addons": [
+                {
+                    "name": "Google Apps (20110828)",
+                    "url": "http://goo-inside.me/gapps/gapps-gb-20110828-signed.zip"
+                },
+                {
+                    "name": "v5 wpa_supplicant (20120206)",
+                    "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/wpa_supplicant_5_20120206.zip"
+                }
+            ]
+        },
+        {
+            "label": "CM7 ROMs for Samsung Galaxy 5",
+            "name": "CyanogenMod 7.2-RC0 (v2.0)",
+            "summary": "(20120206)",
+            "product": "gingerbread-nightly",
+            "modversion": "CyanogenMod-7-20120206-MADTEAM-galaxy5",
+            "incremental": "20120206",
+            "device": "galaxy5",
+            "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/update-cm-7-20120206-MADTEAM-galaxy5.zip",
+            "addons": [
+                {
+                    "name": "v2.0 (20120206) Hotfix 2",
+                    "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5-v2.0-hotfix2.zip"
+                },
+                {
+                    "name": "Google Apps (20110828)",
+                    "url": "http://goo-inside.me/gapps/gapps-gb-20110828-signed.zip"
+                },
+                {
+                    "name": "v5 wpa_supplicant (20120206)",
+                    "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/wpa_supplicant_5_20120206.zip"
+                }
+            ]
+        },
+        {
+            "label": "Miscellaneous for Samsung Galaxy 5",
+            "name": "Hotfix Updater",
+            "summary": "Use this to install new hotfixes separately\n*DO remember that you must manually choose ONE hotfix from the list\n*DON'T select mismatching hotfix versions",
+            "device": "galaxy5",
+            "visible": "true",
+            "url": "http://goo-inside.me/devs/psyke83/roms/chooser.zip",
+            "addons": [
+                {
+                    "name": "v2.0 (20120206) Hotfix 2",
+                    "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/galaxy5-v2.0-hotfix2.zip"
+                }
+            ]
+        },
+        {
+            "label": "Miscellaneous for Samsung Galaxy 5",
+            "name": "WPA Supplicant Chooser",
+            "summary": "Lets you install the v5 or v6 WPA supplicant for wifi testing purposes\n*Note: there's no need to wipe your dalvik cache",
+            "device": "galaxy5",
+            "url": "http://goo-inside.me/devs/psyke83/roms/chooser.zip",
+            "addons": [
+                {
+                    "name": "v6 wpa_suppliant (20120206)",
+                    "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/wpa_supplicant_6_20120206.zip"
+                },
+                {
+                    "name": "v5 wpa_supplicant (20120206)",
+                    "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/wpa_supplicant_5_20120206.zip"
+                }
+            ]
+        },
+        {
+            "label": "Miscellaneous for Samsung Galaxy 5",
+            "name": "ClockworkMod 5.0.2.8",
+            "summary": "This will flash the full version (not the readonly_recovery version) to your recovery partition\n*Note: there's no need to wipe your dalvik cache",
+            "product": "gingerbread-recovery",
+            "modversion": "5.0.2.8",
+            "incremental": "5.0.2.8",
+            "device": "galaxy5",
+            "url": "http://goo-inside.me/devs/psyke83/roms/galaxy5/recovery/recovery-clockwork-5.0.2.8-galaxy5.img.zip"
+        },
+        {
+            "label": "CM7 ROMs for Huawei U8160/U8180",
+            "name": "CyanogenMod 7.2-RC0",
+            "summary": "[fake update test] (20120204)",
+            "visible": "false",
+            "product": "gingerbread-nightly",
+            "modversion": "CyanogenMod-7-20120204-MADTEAM-U8160",
+            "incremental": "20120204",
+            "device": "u8160",
+            "url": "http://goo-inside.me/devs/psyke83/roms/u8160/update-cm-7-20120204-MADTEAM-U8160.zip",
+            "addons": [
+                {
+                    "name": "Google Apps (20110828)",
+                    "url": "http://goo-inside.me/gapps/gapps-gb-20110828-signed.zip"
+                }
+            ]
+        },
+        {
+            "label": "CM7 ROMs for Huawei U8160/U8180",
+            "name": "CyanogenMod 7.2-RC0 (v0.9)",
+            "summary": "Nightly Release (20120203)",
+            "product": "gingerbread-nightly",
+            "modversion": "CyanogenMod-7-20120203-MADTEAM-U8160",
+            "incremental": "20120203",
+            "device": "u8160",
+            "url": "http://goo-inside.me/devs/psyke83/roms/u8160/update-cm-7-20120203-MADTEAM-U8160.zip",
+            "addons": [
+                {
+                    "name": "Google Apps (20110828)",
+                    "url": "http://goo-inside.me/gapps/gapps-gb-20110828-signed.zip"
+                }
+            ]
+        },
+        {
+            "label": "CM9 ROMs for Huawei U8160/U8180",
+            "name": "CyanogenMod 9 (alpha 2)",
+            "summary": "(20111129)\nDeveloper preview - not recommended",
+            "visible": "false",
+            "product": "ics-nightly",
+            "modversion": "CyanogenMod-9-20111129-MADTEAM-U8160",
+            "incremental": "20111129",
+            "device": "u8160",
+            "url": "http://goo-inside.me/devs/psyke83/roms/u8160/update-cm-9-20111129-MADTEAM-U8160.zip",
+            "addons": [
+                {
+                    "name": "Google Apps for ICS (20120201)",
+                    "url": "http://goo-inside.me/gapps/gapps-ics-20120201-signed.zip.zip"
+                }
+            ]
+        }
+    ]
+}
